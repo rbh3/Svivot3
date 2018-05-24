da0d5b40ae0f2faf54556d1b5a0436be77bfa317
Ravid Ben-Harosh <ravid.benharosh@gmail.com>
2018-05-24 12:17:36 +0300

    middleware

--- Modules/POI.js
+++ Modules/POI.js
@@ -1,13 +1,53 @@
 var express = require('express');
-
+var morgan = require('morgan');
+var jwt = require('jsonwebtoken');
 var router = express.Router();
 var DButilsAzure = require('../DButils');
 module.exports = router;
+// use morgan to log requests to the console
+router.use(morgan('dev'));
+
+var  superSecret = "dorRavid12"; // secret variable
+
+
+//Check Token
+router.use('/reg', function (req, res, next) {
+    // check header or url parameters or post parameters for token
+    var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+    // decode token
+    if (token) {
+
+        // verifies secret and checks exp
+        jwt.verify(token, superSecret, function (err, decoded) {
+            if (err) {
+                return res.json({ success: false, message: 'Failed to authenticate token.' });
+            } else {
+                // if everything is good, save to request for use in other routes
+                // get the decoded payload and header
+                var decoded = jwt.decode(token, {complete: true});
+                req.decoded= decoded;
+                console.log(decoded.header);
+                console.log(decoded.payload)
+                next();
+            }
+        });
+
+    } else {
+
+        // if there is no token
+        // return an error
+        return res.status(403).send({
+            success: false,
+            message: 'No token provided.'
+        });
+    }
+})
 
 //POIs
 
 
-router.get('/FavoritesByUsername/:un', function(req,res,next) {
+router.get('/reg/FavoritesByUsername/:un', function(req,res,next) {
    console.log("CHECKKK SUCCESSSS")
     var un = req.params.un;
     DButilsAzure.execQuery("select POIid from FavoritePoi where Username='" + un + "' order by Date desc").then(function(response){
--- server.js
+++ server.js
@@ -14,45 +14,11 @@ var POI = require('./Modules/POI');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/users', users);
-app.use('/reg/POI', POI);
-// use morgan to log requests to the console
-app.use(morgan('dev'));
+app.use('/POI', POI);
 
-var  superSecret = "dorRavid12"; // secret variable
 
-app.use('/reg', function (req, res, next) {
-    console.log("INSIDEEEEEE000")
-    // check header or url parameters or post parameters for token
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
-    // decode token
-    if (token) {
 
-        // verifies secret and checks exp
-        jwt.verify(token, superSecret, function (err, decoded) {
-            if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-            } else {
-                // if everything is good, save to request for use in other routes
-                // get the decoded payload and header
-                var decoded = jwt.decode(token, {complete: true});
-                req.decoded= decoded;
-                console.log(decoded.header);
-                console.log(decoded.payload)
-                next();
-            }
-        });
-
-    } else {
-
-        // if there is no token
-        // return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-    }
-})
 
 var port = 3000;
 app.listen(port, function () {
